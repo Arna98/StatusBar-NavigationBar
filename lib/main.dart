3e7a83b87e451b8527dedd55c350b03509709196
Alireza Naseri <alireza.real98@gmail.com>
2022-09-10 21:33:39 +0430

    update Theme Data & Write AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,15 +15,33 @@ class MyApp extends StatelessWidget {
           fontFamily: "pobbins",
           textTheme: const TextTheme(
               headline1: TextStyle(
-                  color: Colors.black,
+                  color: Colors.white,
                   fontFamily: "pobbins",
                   fontWeight: FontWeight.w700,
-                  fontSize: 25),
+                  fontSize: 18),
               bodyText1: TextStyle(
                   color: Colors.white,
                   fontFamily: "pobbins",
                   fontWeight: FontWeight.w500,
-                  fontSize: 17))),
+                  fontSize: 15))),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          elevation: 1,
+          title: Center(child: Text("Stauts Bar & Navigation Bar", style: Theme.of(context).textTheme.headline1)),
+          ),
     );
   }
 }
