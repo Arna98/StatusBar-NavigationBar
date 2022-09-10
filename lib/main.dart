68cf1bebc70b90d9a56abeb6b178976bcbf67010
Alireza Naseri <alireza.real98@gmail.com>
2022-09-10 21:12:11 +0430

    Write theme Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Home',
+      theme: ThemeData(
+          fontFamily: "pobbins",
+          textTheme: const TextTheme(
+              headline1: TextStyle(
+                  color: Colors.black,
+                  fontFamily: "pobbins",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25),
+              bodyText1: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "pobbins",
+                  fontWeight: FontWeight.w500,
+                  fontSize: 17))),
     );
   }
 }
