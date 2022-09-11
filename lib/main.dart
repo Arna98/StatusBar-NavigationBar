4f3ba2b7739c8fa26b662ce32856ced3631978b4
Alireza Naseri <alireza.real98@gmail.com>
2022-09-11 15:43:14 +0430

    Write body & toggle Button for Hide status bar and navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,19 +30,99 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<bool> _selection = [true, false, false, false];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          elevation: 1,
-          title: Center(child: Text("Stauts Bar & Navigation Bar", style: Theme.of(context).textTheme.headline1)),
-          ),
+        backgroundColor: Colors.blueAccent,
+        elevation: 1,
+        title: Center(
+            child: Text("Stauts Bar & Navigation Bar",
+                style: Theme.of(context).textTheme.headline1)),
+      ),
+      body: Column(children: [
+        Text("Hide Staus Bar & Navigation Bar :",
+            style: Theme.of(context).textTheme.bodyText1),
+        ToggleButtons(
+            isSelected: _selection,
+            color: Colors.black,
+            selectedColor: Colors.white,
+            fillColor: Colors.blue.shade900,
+            renderBorder: false,
+            onPressed: (index) {
+              setState(() {
+                for (int myIndex = 0; myIndex < _selection.length; myIndex++) {
+                  if (myIndex == index) {
+                    _selection[myIndex] = true;
+                  } else {
+                    _selection[myIndex] = false;
+                  }
+                }
+              });
+              switch (index) {
+                case 0:
+                  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+                      overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+                  break;
+                case 1:
+                  SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+                  break;
+                case 2:
+                  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+                  break;
+                case 3:
+                  SystemChrome.setEnabledSystemUIMode(
+                      SystemUiMode.immersiveSticky);
+                  break;
+              }
+            },
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("None",
+                    style: TextStyle(
+                        fontFamily: "pobbins",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("LeanBack",
+                    style: TextStyle(
+                        fontFamily: "pobbins",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Immersive",
+                    style: TextStyle(
+                        fontFamily: "pobbins",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("ImmersiveSticky",
+                    style: TextStyle(
+                        fontFamily: "pobbins",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500)),
+              ),
+            ]),
+      ]),
     );
   }
 }
